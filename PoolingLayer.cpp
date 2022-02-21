14d54cd7f5050f12e90fe4fa026df6c3e0bab2f1
Nikolay Tverdokhleb <kolyatverdokhleb@gmail.com>
2022-02-21 15:09:46 +0100

    Moved to Tensor

--- PoolingLayer.cpp
+++ PoolingLayer.cpp
@@ -3,80 +3,90 @@
 
 void PoolingLayer::feedForward()
 {
-    for (unsigned int i = 0; i < _inputLayers.size(); ++i){
-        outputLayers.push_back(maxPool(_inputLayers[i], _backPropMapping[i]));
+    // Iterating over all layers in input tensor
+    for (unsigned int i = 0; i < _inputNumLayers; ++i){
+        // Doing max pooling of each layer
+        maxPoolLayer(i);
     }
 }
 
-Matrix PoolingLayer::maxPool(Matrix &toBePooled, std::vector<unsigned int>& backPropMapping)
+void PoolingLayer::maxPoolLayer(unsigned int currentLayerInd)
 {
-    unsigned int number_of_rows = toBePooled.get_rows()   /_stride - (_blockSize - _stride);
-    unsigned int number_of_cols = toBePooled.get_columns()/_stride - (_blockSize - _stride);
-    for (int i = 0; i < _inputLayers.size(); ++i) {
-        _backPropMapping[i].resize(number_of_rows*number_of_cols);
+    for (int i = 0; i < _inputNumLayers; ++i) {
+        _backPropMapping[i].resize(_outputNumRows*_outputNumCols);
     }
 
-    Matrix pooledMat(number_of_rows, number_of_cols);
     std::vector<float>        regionOfInterestVals;
     std::vector<unsigned int> regionOfInterestPos;
     regionOfInterestVals.reserve(_blockSize*_blockSize);
     regionOfInterestPos.reserve(_blockSize*_blockSize);
 
-    for (unsigned int i = 1; i <= number_of_rows; ++i) {
-        for (unsigned int j = 1; j <= number_of_cols; ++j) {
-            for (unsigned int k = 1; k <= _blockSize; ++k) {
-                for (unsigned int r = 1; r <= _blockSize; ++r) {
-                    unsigned int row = (i-1)*_stride + k;
-                    unsigned int clm = (j-1)*_stride + r;
-                    regionOfInterestVals.push_back(toBePooled(row, clm));
-                    regionOfInterestPos.push_back((row - 1) + (clm-1)*toBePooled.get_columns());
+    for (unsigned int i = 0; i < _outputNumRows; ++i) {
+        for (unsigned int j = 0; j < _outputNumCols; ++j) {
+            for (unsigned int k = 0; k < _blockSize; ++k) {
+                for (unsigned int r = 0; r < _blockSize; ++r) {
+                    unsigned int row = i*_stride + k;
+                    unsigned int clm = j*_stride + r;
+                    regionOfInterestVals.push_back(_inputLayers(row, clm, currentLayerInd));
+                    regionOfInterestPos.push_back(row + clm*_inputNumCols);
                 }
             }
-            pooledMat(i, j) = *std::max_element(regionOfInterestVals.begin(), regionOfInterestVals.end());
-            backPropMapping[(i - 1) + (j - 1)*number_of_cols] = regionOfInterestPos[std::max_element(regionOfInterestVals.begin(),regionOfInterestVals.end()) - regionOfInterestVals.begin()];
+            outputLayers(i, j, currentLayerInd) = *std::max_element(regionOfInterestVals.begin(), regionOfInterestVals.end());
+            _backPropMapping[currentLayerInd][i + j*_outputNumCols]
+            = regionOfInterestPos[std::max_element(regionOfInterestVals.begin(),regionOfInterestVals.end()) - regionOfInterestVals.begin()];
 
             regionOfInterestVals.resize(0);
             regionOfInterestPos.resize(0);
         }
     }
 
-    return pooledMat;
 }
 
-std::vector<Matrix> PoolingLayer::backPropogation(const std::vector<Matrix>& lossGradOfOutput)
+Tensor PoolingLayer::backPropogation(Tensor& lossGradOfOutput)
 {
-    std::vector<Matrix> backPropGrad;
-    backPropGrad.reserve(_inputLayers.size());
-    for (int i = 0; i < _inputLayers.size(); ++i) {
-        backPropGrad.push_back(unpool(i, const_cast<Matrix &>(lossGradOfOutput[i])));
+    Tensor backPropGrad = Tensor(_inputNumRows, _inputNumCols, _inputNumLayers);
+    // Iterating over all layers in input tensor
+    for (unsigned int i = 0; i < _inputNumLayers; ++i){
+        // Doing max unpooling of each layer
+        unpool(backPropGrad, lossGradOfOutput, i);
     }
     return backPropGrad;
 }
 
-Matrix PoolingLayer::unpool(unsigned int i, Matrix& lossGradOfOutput)
+void PoolingLayer::unpool(Tensor& backPropGrad, Tensor& lossGradOfOutput, unsigned int currentLayer)
 {
-    Matrix unpooled(_inputLayers[i].get_rows(), _inputLayers[i].get_columns());
-    for (unsigned int j = 0; j < _backPropMapping[i].size(); ++j) {
-        unsigned int fit_i_small = j % outputLayers[i].get_columns() + 1;
-        unsigned int fit_j_small = j / outputLayers[i].get_columns() + 1;
-        unsigned int fit_i_big   = _backPropMapping[i][j] % _inputLayers[i].get_columns() + 1;
-        unsigned int fit_j_big   = _backPropMapping[i][j] / _inputLayers[i].get_columns() + 1;
+    for (unsigned int j = 0; j < _backPropMapping[currentLayer].size(); ++j) {
+        unsigned int fit_i_small = j % _outputNumRows;
+        unsigned int fit_j_small = j / _outputNumCols;
+        unsigned int fit_i_big   = _backPropMapping[currentLayer][j] % _inputNumRows;
+        unsigned int fit_j_big   = _backPropMapping[currentLayer][j] / _inputNumCols;
 
-        unpooled(fit_i_big, fit_j_big) += lossGradOfOutput(fit_i_small, fit_j_small);
+        backPropGrad(fit_i_big, fit_j_big, currentLayer) += lossGradOfOutput(fit_i_small, fit_j_small, currentLayer);
     }
-    return unpooled;
 }
 
 // Testing of that layer:
-//std::vector<Matrix> testLayers = setTestData(2, 8, 8);
+//std::string image_path = "../python/images_batch_1/airbus_s_000662.png";
+//cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR);
+//int rows = 8; int cols = 8; int layers = 2;
+//Tensor testLayers(rows, cols, layers);
+//float temp = 1.0f;
+//for (int k = 0; k < layers; k++){
+//for (int i = 0; i < rows; i++){
+//for (int j = 0; j < cols; j++){
+//testLayers(i,j,k) = temp;
+//temp += 1.f;
+//}
+//}
+//}
 //std::cout << "Test matrix = " << std::endl;
-//std::cout << testLayers[1] << std::endl;
+//std::cout << testLayers << std::endl;
 //PoolingLayer poolingLayer(testLayers, 2, 2);
 //poolingLayer.feedForward();
 //std::cout << "Test matrix after pooling = "  << std::endl;
-//std::cout << poolingLayer.outputLayers[1] << std::endl;
+//std::cout << poolingLayer.outputLayers << std::endl;
 //
-//std::vector<Matrix> backPropGrad = setTestData(2, 4, 4);
-//std::vector<Matrix> backProp = poolingLayer.backPropogation(backPropGrad);
+//Tensor backPropGrad = poolingLayer.outputLayers;
+//Tensor backProp = poolingLayer.backPropogation(backPropGrad);
 //std::cout << "Back propageted gradient = "  << std::endl;
-//std::cout << backProp[1] << std::endl;
+//std::cout << backProp << std::endl;
--- PoolingLayer.h
+++ PoolingLayer.h
@@ -1,19 +1,23 @@
 #ifndef GPU_PROJECT_POOLINGLAYER_H
 #define GPU_PROJECT_POOLINGLAYER_H
 
-#include <iostream>
-#include <string>
-#include <sstream>
-#include "Matrix.h"
+#include "Tensor.h"
 
 class PoolingLayer {
 
 public:
-    PoolingLayer(const std::vector<Matrix>& inputLayers, unsigned int stride, unsigned int blockSize):
+    PoolingLayer(const Tensor& inputLayers, unsigned int stride, unsigned int blockSize):
         _inputLayers(inputLayers), _stride(stride), _blockSize(blockSize)
     {
-        outputLayers.reserve(inputLayers.size());
-        _backPropMapping.resize(_inputLayers.size());
+        // Getting dimensions of input tensor
+        _inputNumLayers = inputLayers.get_layers();
+        _inputNumRows = inputLayers.get_rows();
+        _inputNumCols = inputLayers.get_cols();
+        // Calculating dimensions of output tensor
+        _outputNumRows = _inputNumRows/_stride - (_blockSize - _stride);
+        _outputNumCols = _inputNumCols/_stride - (_blockSize - _stride);
+        outputLayers = Tensor(_outputNumRows, _outputNumCols, _inputNumLayers);
+        _backPropMapping.resize(_inputNumLayers);
 
         std::cout << "Pooling parameters: stride = "  << stride
                   << ", size of the pooling block = " << blockSize << std::endl;
@@ -24,18 +28,24 @@ public:
         std::cout << std::endl;
     };
 
-    std::vector<Matrix> outputLayers;
+    Tensor outputLayers;
     void feedForward();
-    std::vector<Matrix> backPropogation(const std::vector<Matrix>& lossGradOfOutput);
-    Matrix unpool(unsigned int i, Matrix& lossGradOfOutput);
+    Tensor backPropogation(Tensor& lossGradOfOutput);
+
 
 private:
-    unsigned int _stride;
-    unsigned int _blockSize;
-    std::vector<Matrix> _inputLayers;
+    unsigned int _stride{};
+    unsigned int _blockSize{};
+    unsigned int _inputNumLayers{};
+    unsigned int _inputNumRows{};
+    unsigned int _inputNumCols{};
+    unsigned int _outputNumRows{};
+    unsigned int _outputNumCols{};
+    Tensor _inputLayers;
     std::vector<std::vector<unsigned int>> _backPropMapping;
 
-    Matrix maxPool(Matrix& toBePooled, std::vector<unsigned int>& backPropMapping);
+    void maxPoolLayer(unsigned int currentLayerInd);
+    void unpool(Tensor& backPropGrad, Tensor& lossGradOfOutput, unsigned int currentLayer);
 };
 
 #endif //GPU_PROJECT_POOLINGLAYER_H
